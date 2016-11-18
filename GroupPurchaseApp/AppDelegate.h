dfdf3a1e170feaf41654eeb84aa6b0903d167360
scholarin <apple2@192.168.0.103>
2016-11-18 22:38:19 +0800

    Initial Commit

---
+++ GroupPurchaseApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GroupPurchaseApp
+//
+//  Created by Apple2 on 2016/11/18.
+//  Copyright © 2016年 Mosin Nagant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
